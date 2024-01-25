155129b22c1fa67859b4622ddd36f4d37603ad40
dylsteck <dylancsteck@gmail.com>
2024-01-24 19:32:32 -0500

    all content is done for thoughts on building a farcaster client
    just need to add og image then a small touch up(non article related) then can merge with main and publish

--- src/components/mdx-components/ul.tsx
+++ src/components/mdx-components/ul.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
 
 export function UL({ children }: { children: ReactNode }) {
-  return <ul className="my-1 list-none list-inside">{children}</ul>;
+  return <ul className="list-inside list-disc flex flex-col">{children}</ul>;
 }
