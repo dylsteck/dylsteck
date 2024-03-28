b546407e3a4323acfd2b9c13c7a037186f8ca715
dylsteck <dylancsteck@gmail.com>
2024-03-27 19:04:23 -0700

    fix: homepage left padding and react-farcaster-embed implementation

--- src/components/mdx-components/cast.tsx
+++ src/components/mdx-components/cast.tsx
@@ -1,37 +1,12 @@
 import React, { ReactNode, useEffect, useState } from 'react';
-import { FarcasterEmbed as FarcasterEmbedAsync } from 'react-farcaster-embed';
-import "react-farcaster-embed/dist/styles.css";
+// @ts-ignore
+import { FarcasterEmbed } from "react-farcaster-embed/dist/client";
 
 const Cast = ({ url, username, hash }: { url?: string, username?: string, hash?: string}) => {
-  const [embedContent, setEmbedContent] = useState<ReactNode | null>(null);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const fetchEmbed = async () => {
-      try {
-        const content = await FarcasterEmbedAsync({ url, username, hash });
-        setEmbedContent(content);
-      } catch (err) {
-        setError(err as unknown as string);
-      }
-    };
-
-    fetchEmbed();
-  }, [url, username, hash]);
-
-  // TODO: style the loading and error states
-  if (error) {
-    return <div>Error loading embed: {error}</div>;
-  }
-  if (!embedContent) {
-    return <div>Loading...</div>;
-  }
 
   return (
     <div className="max-w-[95%] md:max-w-[50%] pt-4 pb-4">
-      <div>
-        {embedContent}
-      </div>
+      <FarcasterEmbed url={url} username={username} hash={hash} />
     </div>
   );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import "react-farcaster-embed/dist/styles.css";
 import { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
