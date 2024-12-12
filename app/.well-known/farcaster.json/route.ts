15cb0904318c6c342f5ce2ce2fff9ae733321cf0
dylsteck <dylancsteck@gmail.com>
2024-12-12 16:41:48 -0500

    chore: update `accountAssociation` to go with the previous commit

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -4,8 +4,8 @@ export async function GET() {
   const config = {
     "accountAssociation": {
       "header": "eyJmaWQiOjYxNiwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDVFNzlGNjkwY2NENDIwMDdENUEwYUQ2NzhDRDQ3NDc0MzM5NDAwRTMifQ",
-      "payload": "eyJkb21haW4iOiJ3d3cuZHlsYW5zdGVjay5jb20ifQ",
-      "signature": "MHg5ZDEzODdhODkxMTk5YzliMDg3ZDhiZWZlMTNjYTk1ZGU5NGU0MTJhNmMzMDIwMzg3YzUwZjllNGIyNTM3NDliMmExZDg3MTJjYWRhMTQyZTg0NzM4NjA0NjNkM2I0YzczMGZiODM0MDZjMWEwZmEwZWM4NzQ5ZThhZjI2Zjk4ODFi"
+      "payload": "eyJkb21haW4iOiJkeWxhbnN0ZWNrLmNvbSJ9",
+      "signature": "MHg3ZWQ3OGZiNmE4NDkwYTZiNTUxNWI3NjY2NTlmMTZmMzkzMmUyMmUwYWY0M2Q0NTY3MWM4OGMxMTc1NGFlNTk4MmIwMGQ0ZWRiM2Y0Mjg0NzkxNGYwMTI4ZjE0NDE1YjEzYWM5NGY3MTMwMGJkNzY2MGE3ZTEyZmRjZjNlY2I3OTFj"
     },
     frame: {
       version: "0.0.1",
