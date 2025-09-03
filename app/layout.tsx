a92d4f8b3f78c1a42240a4e9aa35dcadab8e3bbf
dylsteck <dylancsteck@gmail.com>
2025-09-02 22:08:48 -0400

    wip: adds carousel

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,16 +65,16 @@ export default function RootLayout({
       <Head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </Head>
-      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
+      <body className="antialiased max-w-xl mx-4 lg:mx-auto">
         <Navbar />
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+        <main className="flex-auto min-w-0 pt-20 pb-20 flex flex-col px-2 md:px-0">
           <MiniKitProvider chain={null as any}>
             <FrameProvider>
               {children}
             </FrameProvider>
           </MiniKitProvider>
-          <Footer />
         </main>
+        <Footer />
         <Script
           strategy="afterInteractive"
           src="https://dylsteck-analytics.up.railway.app/umami.js"
